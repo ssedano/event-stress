01e291c183070c9b558484600edfc1f2941afe7b
Serafin Sedano <serafin.sedano@abiquo.com>
2013-04-26 18:58:30 +0200

    Queue creation update

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.novemberain/langohr "1.0.0-beta13"]
+                 [com.novemberain/langohr "1.0.0-beta13" :exclusions [com.google.guava/guava]]
                  [clj-time "0.5.0"]
                  [com.abiquo/event-model-transport "1.0-SNAPSHOT"]]
   :jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]
--- src/event_stress/core.clj
+++ src/event_stress/core.clj
@@ -1,41 +1,63 @@
 (ns event-stress.core
   (:gen-class)
   (:require [langohr.core :as rmq]
+            [langohr.exchange :as le]
             [langohr.channel :as lch]
             [langohr.basic :as lb]
+            [langohr.queue :as lq]
             [clj-time.core :as t]
             [clj-time.coerce :as r])
   (:import com.abiquo.event.model.enumerations.Severity
            com.abiquo.event.model.enumerations.EntityAction)
-  (:use [clojure.string :only [lower-case]]))
+  (:use [clojure.string :only [lower-case capitalize]]))
 
-(def entities (map #(.getSimpleName %) (filter #(not (.isEnum %)) (.getDeclaredClasses EntityAction))))
 
-(def entityClass (map #(Class/forName (.getName %)) (filter #(not (.isEnum %)) (.getDeclaredClasses EntityAction))))
+(def exchange "abiquo.tracer")
+
+(def queue "abiquo.tracer.traces")
+
+(def entities (map #(.getSimpleName %) (remove #(or (.isEnum %) (= (.getSimpleName %) "") (= (.getSimpleName %) "Action")) (.getDeclaredClasses EntityAction))))
+
+(def entityClass (map #(Class/forName (.getName %)) (remove #(.isEnum %) (.getDeclaredClasses EntityAction))))
+
+(def package "com.abiquo.event.model.enumerations.EntityAction$")
 
 (defn user [] (rand-nth ["SYSTEM", (str "/admin/enterprises/1/users/" (rand-int 10))]))
 
 (defn severity [] (rand-nth (Severity/values)))
 
-(defn entity [] (rand-nth entities))
-
 (defn action [e]
-  (let [class-name (str "com.abiquo.event.model.enumerations.EntityAction$" e)
+  (let [class-name (str package e)
         clazz (Class/forName class-name)
         fields (map #(.getName %) (.getDeclaredFields clazz))]
-      (rand-nth fields)))
+    (rand-nth fields)))
 
 (defn entity-id [en]
   (str "/admin/" (lower-case en) "/" (rand-int 10)))
 
+
+(defn details [sev ent act]
+  (let [m (str "get" (capitalize (lower-case sev)) "keys")
+        class-name (str package ent)
+        clazz (Class/forName class-name)
+        field (. clazz getDeclaredField act)
+        method (. field get m)
+        fields (. (. method getClass) getDeclaredMethod m nil)
+        k (map #(.name %) (. fields invoke method (make-array Object 0)))]
+    (vec (interleave (take (count k) (repeatedly #(str (rand-int 100)))) (repeat (count k) ",")))))
+
 (defn event [] 
-  (let [e (entity)
-        a (action e)]
-    (str "{\"timestamp\":" (r/to-long (t/now)) ",\"user\":\"" (user) "\",\"severity\":\"" (severity) "\",\"source\":\"ABIQUO_SERVER\",\"action\":\"" a "\",\"type\":\"" e "\",\"entityIdentifier\":\"" (entity-id e) "\",\"details\":{\"detail\":[]}}")))
+  (let [e (rand-nth entities)
+        a (action e)
+        s (severity)
+        d (details s e a)]
+    (str "{\"timestamp\":" (r/to-long (t/now)) ",\"user\":\"" (user) "\", \"enterprise\":\"" (user) "\",\"severity\":\"" s "\",\"source\":\"ABIQUO_SERVER\",\"action\":\"" a "\",\"type\":\"" e "\",\"entityIdentifier\":\"" (entity-id e) "\",\"details\":{\"detail\": " d "}}")))
 
 (defn stress [n]
-  (let [ch (lch/open (rmq/connect))]
-    (repeatedly n  #(lb/publish ch "abiquo.tracer" "abiquo.tracer.traces" (event) :content-type "text/plain"))))
+  (let [conn (rmq/connect)
+        ch (lch/open conn)]
+    (dotimes [i n]
+      (lb/publish ch exchange queue (event) :content-type "text/plain"))))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -44,6 +66,7 @@
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
   (let [n (read-string (first args))]
-    (stress n)
+    (time
+      (stress n))
     (System/exit 0)))
-
+  (event)
